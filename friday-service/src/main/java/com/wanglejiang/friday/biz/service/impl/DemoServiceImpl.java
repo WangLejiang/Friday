eca2961717044568bad45a4172184eb1e0267592
wanglejiang <time2flappy@sina.com>
2022-02-08 09:05:08 +0800

    super mini demo

--- friday-service/src/main/java/com/wanglejiang/friday/biz/service/impl/DemoServiceImpl.java
+++ friday-service/src/main/java/com/wanglejiang/friday/biz/service/impl/DemoServiceImpl.java
@@ -3,11 +3,15 @@ package com.wanglejiang.friday.biz.service.impl;
 import com.wanglejiang.friday.biz.service.DemoService;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class DemoServiceImpl implements DemoService {
 
     @Override
     public String echo(String msg) {
-        return msg;
+
+        return """
+                echo + msg :
+                """ + msg;
     }
 }
