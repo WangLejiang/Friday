e66f723a13e021b90120545445199d2d33f38b26
wanglejiang <time2flappy@sina.com>
2022-02-08 09:20:15 +0800

    super mini demo

--- friday-service/src/main/java/com/wanglejiang/friday/biz/service/DemoService.java
+++ friday-service/src/main/java/com/wanglejiang/friday/biz/service/DemoService.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public interface DemoService {
 
+    /**
+     * echo 用于测试部署联通性的回响接口
+     * @param msg 输入消息
+     * @return msg
+     */
     @GetMapping("/echo/{msg}")
     String echo(@PathVariable("msg") String msg);
 
